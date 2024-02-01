b16221d5c963ec1f26279b3961fe7224e8c24f7d
Seongki Kim <moztiq@gmail.com>
2024-02-01 16:52:40 +0900

    json view counter

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,67 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	DB: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+}
+
+// @ts-ignore
+import home from './home.html';
+
+function handleHome() {
+	return new Response(home, {
+		headers: {
+			'Content-Type': 'text/html;charset=utf-8',
+		},
+	});
+}
+
+function handleNotFound() {
+	return new Response(null, {
+		status: 404,
+	});
+}
+
+function handleBadRequest() {
+	return new Response(null, {
+		status: 400,
+	});
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+	const page = searchParams.get('page');
+	if (!page) {
+		return handleBadRequest();
+	}
+	const kvPage = await env.DB.get(page);
+	let value = 1;
+	if (!kvPage) {
+		await env.DB.put(page, value + '');
+	} else {
+		value = parseInt(kvPage) + 1;
+		await env.DB.put(page, value + '');
+	}
+	return new Response(JSON.stringify({ visits: value }), {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const { pathname, searchParams } = new URL(request.url);
+		switch (pathname) {
+			case '/':
+				return handleHome();
+			case '/visit':
+				return handleVisit(searchParams, env);
+			default:
+				return handleNotFound();
+		}
 	},
 };
